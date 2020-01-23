ad6dee7a3872947257686399b658decf2174d607
Alex <dnjscksdn98@gmail.com>
2020-01-23 18:53:04 +0900

    Add Route & Link

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// Route: 특정 주소에 특정 컴포넌트를 보여준다
+// Link: 특정 링크를 눌렀을때, 다른 경로로 가고 싶을때 - 페이지 로딩 없이
+import { Route, Link } from "react-router-dom";
+
+import Home from "./Home";
+import About from "./About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+      {/* exact: 해당 경로가 완전히 일치할 경우에만 보여준다 */}
+      <Route path="/" component={Home} exact />{" "}
+      <Route path="/about" component={About} exact />
     </div>
   );
 }

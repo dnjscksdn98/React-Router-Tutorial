967c7429bb92d8d634b02243e553440b1eb7fdbb
Alex <dnjscksdn98@gmail.com>
2020-01-26 16:03:21 +0900

    Add quotes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 
+// Router component needs to be the highest parent
+// lets all of the component use the power of Router
 ReactDOM.render(
   <BrowserRouter>
     <App />
@@ -12,7 +14,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
